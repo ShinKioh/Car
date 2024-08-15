a8391270dbb5118b04943dd110fd072069b6c92f
SHIN KI OH <te4328@gmai.com>
2024-08-15 16:41:03 +0900

    update

---
+++ page.js
@@ -0,0 +1,45 @@
+function Carft() {
+  window.open(
+    'https://www.coupang.com/vp/products/7994247015?itemId=22231215746&vendorItemId=89277106293&sourceType=srp_product_ads&clickEventId=134ae750-5ad3-11ef-b976-cef4d2612214&korePlacement=15&koreSubPlacement=1&clickEventId=134ae750-5ad3-11ef-b976-cef4d2612214&korePlacement=15&koreSubPlacement=1&q=%ED%88%AC%EC%8B%BC+nx4+%ED%95%98%EC%9D%B4%EB%B8%8C%EB%A6%AC%EB%93%9C+%ED%8A%B8%EB%A0%81%ED%81%AC%EB%A7%A4%ED%8A%B8&itemsCount=36&searchId=3162be2b39b14021bd51f6cb78ad9114&rank=0'
+  );
+}
+
+function Momoto() {
+  window.open(
+    'https://www.coupang.com/vp/products/7521207046?itemId=19726840542&vendorItemId=86830863537&q=%ED%88%AC%EC%8B%BC+nx4+%ED%95%98%EC%9D%B4%EB%B8%8C%EB%A6%AC%EB%93%9C+%ED%8A%B8%EB%A0%81%ED%81%AC%EB%A7%A4%ED%8A%B8&itemsCount=36&searchId=3162be2b39b14021bd51f6cb78ad9114&rank=3&isAddedCart='
+  );
+}
+
+function AutoCarage() {
+  window.open(
+    'https://smartstore.naver.com/carsmith/products/2481897262?nl-au=f5854edca9374e3bb2c045c2561942fe&NaPm=ct%3Dlzuxnbzc%7Cci%3D8b8b21c1f80e6d98209999a8f8aa19f7a107d8ff%7Ctr%3Dslsl%7Csn%3D265322%7Chk%3D0888345a3e520350ae70ee43fc474e151f67a95c'
+  );
+}
+
+function Ikan() {
+  window.open(
+    'https://smartstore.naver.com/sjmotors1/products/9702010368?nl-au=449d397582c947c3a7145e82cf93b95c&NaPm=ct%3Dlzuxts8g%7Cci%3Da64a88c0b7829fe39eecfec5ddafb12272215751%7Ctr%3Dslsl%7Csn%3D686197%7Chk%3Dde1c355fa1c37708277797d882d3b6af83f66518'
+  );
+}
+
+function Gaon() {
+  window.open('https://brand.naver.com/gaoncarmat/products/5649571609');
+}
+
+function AutoCarageFoot() {
+  window.open(
+    'https://smartstore.naver.com/carsmith/products/6356821460?NaPm=ct%3Dlzuytll4%7Cci%3D42d213e66d89f03f5f7a71689840a42550d6b44e%7Ctr%3Dsls%7Csn%3D265322%7Chk%3D98588e83ffd2482d49cc20bcc26ad06531095154'
+  );
+}
+
+function MatHouse() {
+  window.open(
+    'https://smartstore.naver.com/mathouse/products/730045594?NaPm=ct%3Dlzuyvuls%7Cci%3D6505162beae40fc7c7663b1980edc610758f5f9e%7Ctr%3Dsls%7Csn%3D436407%7Chk%3D66416acfdb9f9f151d452961b4ebf6d4ca2c7aa5'
+  );
+}
+
+function Meyton() {
+  window.open(
+    'https://www.coupang.com/vp/products/7972704440?itemId=22088372238&vendorItemId=89135391240&pickType=COU_PICK&q=%ED%88%AC%EC%8B%BC+nx4+%ED%95%98%EC%9D%B4%EB%B8%8C%EB%A6%AC%EB%93%9C+%EC%BD%94%EC%9D%BC%EB%A7%A4%ED%8A%B8&itemsCount=36&searchId=f0b3af8e92d04bce8f6914de20c4f839&rank=1&isAddedCart='
+  );
+}
